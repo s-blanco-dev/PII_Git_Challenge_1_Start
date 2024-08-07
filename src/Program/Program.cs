d252f2f88ebbbf69062d73367d26de77557e48ea
andreagonzalezucu <andrea.gonzalezr@correo.ucu.edu.uy>
2024-08-07 10:31:11 -0300

    Implementación de la operación feature-subtraction

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,11 @@
 ﻿Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
