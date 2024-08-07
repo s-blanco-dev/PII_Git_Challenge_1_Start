801842fe86c35cecfda950fc9cc0085cac3aab8c
s-blanco-dev <santiago.blancoc@correo.ucu.edu.uy>
2024-08-07 10:27:58 -0300

    Implementación de la operación de multiplicación

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
+﻿public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
