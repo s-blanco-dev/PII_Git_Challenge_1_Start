e38df678e8257fa8b20c2f5484c50e18654558f8
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-08-12 08:21:37 -0300

    Implementacion de la operacion feature-addition

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,11 @@
 ﻿Console.WriteLine(Addition.Add(7, 2));
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
