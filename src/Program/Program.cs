4ff0d682a4ac1f3af2934ca921caba63dd4eb3cd
FelipePaladino407 <felipaladino05@gmail.com>
2024-08-12 08:33:19 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,6 +3,7 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+// Esta clase implementa la operacion Division
 public class Division
 {
     public static double Divide(int a, int b)
